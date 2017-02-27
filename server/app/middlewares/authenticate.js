2eef6e732249b486e1e7d163d382bec0aef02e3d
Olawale Akinseye <brainyfarm@gmail.com>
2017-02-27 04:45:42 +0100

    feature(set-up-test): remove unnecessary code from file

--- server/app/middlewares/authenticate.js
+++ server/app/middlewares/authenticate.js
@@ -1,36 +1,25 @@
 import * as jwt from 'jsonwebtoken';
 
 const authenticate = (req, res, next) => {
-  if (!req.headers['access-token']) {
+  if (!req.headers['access-token'] && !req.body.token && !req.query.token) {
     res.status(403).send({
       message: 'No access token'
     });
   } else {
     const requestToken = req.headers['access-token'] || req.body.token || req.query.token;
-    if (!requestToken) {
-      res.send({
-        message: 'No token to verify'
-      });
-    }
     const decodedToken = jwt.verify(requestToken, 'secret');
     const username = decodedToken.username;
     const firstname = decodedToken.firstname;
     const userId = decodedToken.id;
     const role = decodedToken.role;
 
-    if (!username) {
-      res.status(403).send({
-        message: 'Unauthorized access'
-      });
-    } else {
-      req.user = {
-        userId,
-        username,
-        role,
-        firstname
-      };
-      next();
-    }
+    req.user = {
+      userId,
+      username,
+      role,
+      firstname
+    };
+    next();
   }
 };
 export default authenticate;
