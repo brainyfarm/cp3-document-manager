46dbe3b7ad743a3bd0943d556e914f5af0523ac2
Olawale Akinseye <brainyfarm@gmail.com>
2017-02-28 18:17:42 +0100

    feature(set-up-test): fix merge conflict

--- server/db/migrations/20170203132644-create-role.js
+++ server/db/migrations/20170203132644-create-role.js
@@ -8,14 +8,9 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       title: {
-<<<<<<< HEAD
-        allowNull: false,
-        type: Sequelize.STRING
-=======
         unique: true,
         type: Sequelize.STRING,
         allowNull: false
->>>>>>> cd94bdd62ca09713cf8dde580292c4970088e8a2
       },
       createdAt: {
         allowNull: false,
