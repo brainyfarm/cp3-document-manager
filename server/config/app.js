c5063512d3495de9ba6e056fc094c19cac1b7ee4
Olawale Akinseye <brainyfarm@gmail.com>
2017-03-04 21:41:37 +0100

    add API home route

--- server/config/app.js
+++ server/config/app.js
@@ -15,6 +15,14 @@ const PORT = process.env.PORT || 4040;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+/* Home Route */
+app.use('/', (req, res) => {
+  res.status(200).json({
+    message: 'Epic Document Manager API',
+    apiDocumentation: 'https://github.com/andela-oakinseye/cp3-document-manager',
+    postmanCollection: 'https://www.getpostman.com/collections/461ad21b114af8ccd66c'
+  });
+});
 app.use('/users', userRoutes);
 app.use('/documents', documentRoutes);
 app.use('/roles', roleRoutes);
